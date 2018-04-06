2e1e6d916bbe4880bb1bc6d0f5559944c70dc840
xxxie <280498307@qq.com>
2018-04-06 10:15:07 +0800

    新增服务本编辑器插件以及工厂管理的修改删除方法

---
+++ application/factory/validate/Company.php
@@ -0,0 +1,23 @@
+<?php
+namespace app\factory\validate;
+use think\Validate;
+class Company extends Validate{
+    protected $rule=[
+        'name'=>'require|min:2|max:43',
+        'wages'=>'require',
+        'times'=>'require',
+        'address'=>'require',
+        'img_urls'=>'require',
+        'text'=>'require'
+    ];
+    protected $message=[
+        'name.require'=>'工厂名称不能为空',
+        'name.min'=>'工厂名称不能少于两个字符',
+        'name.max'=>'工厂名称不能超过43字符',
+        'wages.require'=>'薪资标准不能为空',
+        'times.require'=>'工作时间不能为空',
+        'address.require'=>'地址不能为空',
+        'img_urls.require'=>'轮播图不能为空',
+        'text.require'=>'详情介绍不能为空'
+    ];
+}
