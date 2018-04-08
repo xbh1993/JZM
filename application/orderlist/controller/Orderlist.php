2ab9708c0ba910b2915a6240346a3fa81a826834
lzy220 <1160831696@qq.com>
2018-04-08 17:44:47 +0800

    需求列表变动

--- application/orderlist/controller/Orderlist.php
+++ application/orderlist/controller/Orderlist.php
@@ -12,23 +12,7 @@ use think\Request;
 use  \app\common\controller\Main;
 class orderlist extends Main{
     public function lists(){
-        if(request()->isPost()){
-            $d=request()->post();
-            $validate=validate('Center');
-            if(!$validate->check($d)){
-                $msg=$validate->getError();
-                return json_code(0,$msg);
-            }
-            unset($d['images']);
-            $d['img_url']=implode(',',$d['img_url']);
-            try{
-                Db::name('center')->update($d);
-                return json_code(200,'修改成功');
-            }catch (\Exception $e){
-                return json_code(0,'修改失败');
-            }
-        }
-        $lists=Db::name('center')->find();
+        $lists=Db::name('orderlist')->paginate(8);
         if(!empty($lists['img_url'])){
             $lists['img_url']=explode(',',$lists['img_url']);
         }
--- application/orderlist/validate/Orderlist.php
+++ application/orderlist/validate/Orderlist.php
@@ -11,20 +11,20 @@ class Orderlist extends Validate
 {
     protected $rule=[
         'name'=>'require|min:2|max:40',
+        'gender'=>'require',
         'mobile'=>'require|number|min:9|max:11',
-        'address'=>'require',
-        'text'=>'require',
-        'img_url'=>'require'
+        'school'=>'school',
+        'qq'=>'require',
     ];
     protected  $message=[
         'name.require'=>'公司名称不能为空',
         'name.min'=>'公司名称不能少于两个字符',
         'name.max'=>'公司名称不能多于40个字符',
+        'gender.require'=>'性别不能为空',
         'mobile.require'=>'联系方式不能不空',
         'mobile.number'=>'联系方式必须为数字',
-        'text.require'=>'公司详情不能为空',
-        'img_url.require'=>'轮播图不能为空',
-        'address.require'=>'公司地址不能为空',
+        'qq.require'=>'qq不能为空',
+        'school.require'=>'学校不能为空',
         'mobile.min'=>'电话号码最少为9位数字',
         'mobile.max'=>'电话号码最多11位数字'
     ];
